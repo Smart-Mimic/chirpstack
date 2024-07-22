4bdd1e42be2e441c3ba8b89754590deb03de0da6
canemreunal <canemreunal@hotmail.com>
2024-07-22 20:33:09 +0300

    Bump version to 4.6.2

--- api/java/build.gradle.kts
+++ api/java/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.chirpstack"
-version = "4.6.1"
+version = "4.6.2"
 
 repositories {
     mavenCentral()
--- api/python/src/setup.py
+++ api/python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "4.6.1",
+    version = "4.6.2",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
