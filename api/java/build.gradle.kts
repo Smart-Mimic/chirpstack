7d2cf601f6af3688d8e9f431b1ff7c8e0222b2a1
CanEmreUnal <canemreunal@gmail.com>
2025-06-23 17:04:51 +0300

    Bump version to 4.8.2

--- api/java/build.gradle.kts
+++ api/java/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.chirpstack"
-version = "4.8.0"
+version = "4.8.2"
 
 repositories {
     mavenCentral()
--- api/kotlin/build.gradle.kts
+++ api/kotlin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.chirpstack"
-version = "4.8.0"
+version = "4.8.2"
 
 repositories {
     mavenCentral()
